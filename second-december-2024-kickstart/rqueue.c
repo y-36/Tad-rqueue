b9b44f62550c3257422b011f0984e3ceb6d4fbaf
threesix <aaron.aravena@mi.unc.edu.ar>
2024-12-19 16:20:25 -0300

    solution

--- second-december-2024-kickstart/rqueue.c
+++ second-december-2024-kickstart/rqueue.c
@@ -26,31 +26,51 @@ static orientation_t inverse(rqueue_t q) {
 }
 
 rqueue_t rqueue_empty() {
-    //complete here!
+    //complete!
+    rqueue_t q = malloc(sizeof(struct _rqueue_t));
+    if (!q) {
+        perror("Failed to create queue");
+        exit(EXIT_FAILURE);
+    }
+    q->extreme[0] = NULL;
+    q->extreme[1] = NULL;
+    q->orientation = 0; // Initially node q
+    return q;
 }
 
 bool rqueue_is_empty(rqueue_t q) {
-    //complete here!
+    //complete!
+    return q->extreme[0] == NULL && q->extreme[1] == NULL;
 }
 
 static node_t create_node(elem_t e) {
-    //complete here!
+    //complete!
+    node_t node = malloc(sizeof(struct _node_t));
+    if (!node) {
+        perror("Failed to create node");
+        exit(EXIT_FAILURE);
+    }
+    node->value = e;
+    node->links[0] = NULL;
+    node->links[1] = NULL;
+    return node;
 }
 
 rqueue_t rqueue_enqueue(rqueue_t q, elem_t e) {
-    node_t new_node = NULL;
-    node_t aux = NULL;
-    orientation_t prev;
-    orientation_t next;
-    orientation_t lst;
     assert(q != NULL);
-    new_node = create_node(e);
-    if(rqueue_is_empty(q)) {
-        q->extreme[current(q)] = new_node;
-        q->extreme[inverse(q)] = new_node;
-    }
-    else {
-        //complete here!
+    node_t new_node = create_node(e);
+    orientation_t curr = current(q);  
+    orientation_t opp = inverse(q); 
+
+    if (rqueue_is_empty(q)) {
+        // Init the new node in the two extreme
+        q->extreme[0] = new_node;
+        q->extreme[1] = new_node;
+    } else {
+        // add the new node and refresh extreme current 
+        q->extreme[curr]->links[curr] = new_node;
+        new_node->links[opp] = q->extreme[curr];
+        q->extreme[curr] = new_node;
     }
     return q;
 }
@@ -62,15 +82,32 @@ elem_t rqueue_fst(rqueue_t q) {
 }
 
 void rqueue_dequeue(rqueue_t q) {
-    //complete here!
+    //complete!
+    if (rqueue_is_empty(q)) return;
+
+    node_t to_free = q->extreme[current(q)];
+    q->extreme[current(q)] = to_free->links[inverse(q)];
+
+    if (q->extreme[current(q)]) {
+        q->extreme[current(q)]->links[current(q)] = NULL;
+    } else {
+        q->extreme[inverse(q)] = NULL; // Queue is now empty
+    }
+    free(to_free);
 }
 
 void rqueue_revert(rqueue_t q) {
-    //complete here!
+    //complete!
+    q->orientation = inverse(q);
 }
 
 rqueue_t rqueue_destroy(rqueue_t q) {
-    //complete here!
+    //complete!
+    while (!rqueue_is_empty(q)) {
+        rqueue_dequeue(q);
+    }
+    free(q);
+    return NULL;
 }
 
 void rqueue_dump(rqueue_t q) {
